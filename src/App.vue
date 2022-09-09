a25b9764c914453c6ca47821d65da534a545e2de
dmn <87303180+Gb000114@users.noreply.github.com>
2022-09-09 08:54:41 +0800

    滑块

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <custom-citys></custom-citys>
+    <Huakuai></Huakuai>
   </div>
 </template>
 
 <script>
 import CustomCitys from './components/custom-citys.vue';
+import Huakuai from './components/huakuai.vue';
 
 export default {
   components: {
-    CustomCitys
-  },
+    CustomCitys,
+    Huakuai
+},
   data() {
     return {
     }
